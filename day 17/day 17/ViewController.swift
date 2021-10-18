f5df3b46a5ad7866f575ddfadb0e75a2f6698337
maram <maarram36@gmail.com>
2021-10-18 16:50:08 +0300

    done

---
+++ day 17/day 17/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  day 17
+//
+//  Created by Maram Al shahrani on 12/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var myimageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    
+    @IBAction func openPressed(_ sender: Any) {
+        
+      let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker,animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = (info[.editedImage], info[.originalImage]) as? UIImage
+        myimageView.image = image
+        dismiss(animated: true)
+    }
+
+}
+
