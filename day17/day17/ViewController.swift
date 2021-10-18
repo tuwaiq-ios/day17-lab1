9000760c9e0f0d8d4fffdfefca4e4b6130a3f6fc
sarasaud <unix.663@gmail.com>
2021-10-18 16:37:59 +0300

    lap1 day 17 photo

---
+++ day17/day17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  day17
+//
+//  Created by SARA SAUD on 3/12/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
