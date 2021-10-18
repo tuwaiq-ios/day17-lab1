390fb52b233b5ddd9dfd12efd61bcc33453bbe6b
sana alshahrani <me-sa51@hotmail.com>
2021-10-18 18:01:47 +0300

    Sana Alshahrani

---
+++ Day17/Day17/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Day17
+//
+//  Created by Me .. on 12/03/1443 AH.
+//
+
+import UIKit
+class ViewController:UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+
+    @IBOutlet weak var Myimageview: UIImageView!
+    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+    
+    @IBAction func OpenPressed(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+        
+    }
+    
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      
+    let image = (info[ .editedImage], info[.originalImage]) as? UIImage
+    Myimageview.image = image
+    dismiss(animated: true)
+  }
+}
+
