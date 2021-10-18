bfd48bebbdd17d0db18980792f3afcac716289b3
hasaan yahya <h.2011.h@hotmail.com>
2021-10-18 16:41:36 +0300

    done

---
+++ day17/day17/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  day17
+//
+//  Created by Hassan Yahya on 12/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var ourPhotoView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func openPressed(_ sender: Any) {
+        let picker = UIImagePickerController ()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = (info[.editedImage] ?? info[.originalImage]) as? UIImage
+        ourPhotoView.image = image
+        dismiss(animated: true)
+    }
+}
